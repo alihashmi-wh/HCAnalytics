7967e146660888abf91117d91635cacb0c628851
Ali I Hashmi <ahashmi@us.ibm.com>
2021-04-16 14:26:16 -0400

    first attempt at local dev - calculate measure script

---
+++ DashDemo/calc.R
@@ -0,0 +1,37 @@
+datasource = 'synthea'
+dataset = 'synthea_sample_data_csv_apr2020'
+datatype = 'csv'
+
+datapath = file.path(getwd(), datasource, dataset, datatype)
+datafiles = list.files(datapath, pattern = '*.csv')
+
+encounters_file = 'encounters.csv'
+encounters_csv = file.path(datapath, encounters_file)
+encounters = read.csv(encounters_csv)
+
+library(gt)
+library(tidyverse)
+library(glue)
+
+# Define the start and end dates for the data range
+start_date <- "2010-06-07"
+end_date <- "2010-06-14"
+
+# Create a gt table based on preprocessed
+# `sp500` table data
+sp500 %>%
+  filter(date >= start_date & date <= end_date) %>%
+  select(-adj_close) %>%
+  gt() %>%
+  tab_header(
+    title = "S&P 500",
+    subtitle = glue::glue("{start_date} to {end_date}")
+  ) %>%
+  fmt_date(
+    columns = vars(date),
+    date_style = 3
+  ) %>%
+  fmt_currency(
+    columns = vars(open, high, low, close),
+    currency = "USD"
+  ) 
--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # HCAnalytics
-healthcare analytics demonstrator
+Healthcare Analytics Demonstrator
+
+## Approach
+
+1. Identify baseline dataset
+1. Select quality measure
+1. Calculate measure on defined subset
+1. Publish results to web app
